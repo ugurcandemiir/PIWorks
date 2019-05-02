078e6a2a8ee980e28342cdaf44383d246326f8c7
Ugurcan Demir <ugurcandemiir@gmail.com>
2019-05-02 02:40:56 -0400

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,12 @@
+using System;  
+  public class SwapExample  
+   {  
+     public static void Main(string[] args)  
+      {  
+       int  a=5, b=10;
+       int temp = 0;
+       temp = a ;      
+        a=b;
+        b=temp;
+      }  
+  }  
